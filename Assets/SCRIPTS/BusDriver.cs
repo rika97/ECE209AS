642a51d80b972dd181160c67f6bd1dfacc5b9368
Roy <roy.jara224@gmail.com>
2022-06-06 00:55:24 -0700

    bus animation runs

--- Assets/SCRIPTS/BusDriver.cs
+++ Assets/SCRIPTS/BusDriver.cs
@@ -6,20 +6,21 @@ public class BusDriver : MonoBehaviour
 {
 
     private bool toggle = false;
+    private AudioSource myAudioSource;
     // Start is called before the first frame update
     void Start()
     {
-        
+        myAudioSource = this.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (this.GetComponent<AudioSource>().isPlaying && !toggle)
+        if (myAudioSource.isPlaying && !toggle)
         {
             // enable animation
             toggle = true;
-            //this.GetComponent<Animator>().enabled = true;
+            this.GetComponent<Animator>().enabled = true;
             this.GetComponent<Animator>().Play(0);
         }
         //if(this.GetComponent<Animator>().end)
