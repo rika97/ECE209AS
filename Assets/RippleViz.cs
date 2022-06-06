83fc04b60b521f16a104427bb10c5d67a9f08841
Daisy <penguinsliding4@gmail.com>
2022-06-05 18:56:07 -0700

    auto set ripples or spheres, use volumegrabber for volume scaling, fix restarting ripples

--- Assets/RippleViz.cs
+++ Assets/RippleViz.cs
@@ -6,8 +6,8 @@ using UnityEngine;
 
 public class RippleViz : MonoBehaviour
 {
-    public bool showRings = true;
-    public bool showSpheres = true;
+    private bool showRings;
+    private bool showSpheres;
     private GameObject ringModel;
     private GameObject sphereModel;
     private float rippleTime;
@@ -19,6 +19,7 @@ public class RippleViz : MonoBehaviour
     private Queue<GameObject> spheres = new Queue<GameObject>();
     private float scale;
     private AudioSource audioSource;
+    private SoundVolumeGrabber volumeGrabber;
 
 
     // Start is called before the first frame update
@@ -30,13 +31,23 @@ public class RippleViz : MonoBehaviour
         Bounds bounds = gameObject.GetComponent<Renderer>().bounds;
         scale = (new float[] { bounds.size.x, bounds.size.y, bounds.size.z }).Max()*1.5f;
         audioSource = gameObject.GetComponent<AudioSource>();
+        volumeGrabber = gameObject.GetComponent<SoundVolumeGrabber>();
+        
+        if (bounds.center.y - bounds.extents.y < .1f)
+        {
+            showRings = true;
+            showSpheres = false;
+        } else
+        {
+            showRings = false;
+            showSpheres = true;
+        }
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        print(audioSource.isPlaying);
         if (rippleOn)
         {
             if (!audioSource.isPlaying)
@@ -48,10 +59,8 @@ public class RippleViz : MonoBehaviour
                 float t = Time.time;
                 if (showRings)
                 {
-                    //Debug.LogWarning("showing rings now--- in " + gameObject.name);
                     if (t - rippleTime > 1)
                     {
-                        //int nRings = Convert.ToInt32(Math.Floor(Mathf.Min(volume, .99f) / 0.2)) + 1;
                         GameObject newRing = CreateRing(5);
                         rings.Enqueue(newRing);
                         rippleTime = t;
@@ -73,20 +82,15 @@ public class RippleViz : MonoBehaviour
                 {
                     if (t - sphereTime > 4)
                     {
-                        print("A");
-                        //int nSpheres = Convert.ToInt32(Math.Floor(Mathf.Min(volume, .99f) / 0.5)) + 1;
                         GameObject newSphere = CreateSphere(8);
                         spheres.Enqueue(newSphere);
                         sphereTime = t;
                         activeSpheres++;
-                        print("B");
                         if (activeSpheres > 2)
                         {
-                            print("C");
                             GameObject oldestSphere = spheres.Dequeue();
                             Destroy(oldestSphere);
                             activeSpheres -= 1;
-                            print("D");
                         }
                     }
 
@@ -105,14 +109,8 @@ public class RippleViz : MonoBehaviour
     public void BeginRipple()
     {
         rippleOn = true;
-        //GameObject newRing = CreateRing();
-        //GameObject newSphere = CreateSphere();
-        //rings.Enqueue(newRing);
-        //spheres.Enqueue(newSphere);
         rippleTime = Time.time - 1f;
         sphereTime = Time.time - 4f;
-        //activeRings = 1;
-        //activeSpheres = 1;
     }
 
     public void StopRipple()
@@ -128,28 +126,30 @@ public class RippleViz : MonoBehaviour
         }
         rings.Clear();
         spheres.Clear();
+        activeRings = 0;
+        activeSpheres = 0;
     }
 
     GameObject CreateRing(int lifespan)
     {
         Vector3 pos = gameObject.transform.position;
-        float volume = audioSource.volume;
+        float volume = volumeGrabber.postLoudness();
         GameObject newRing = Instantiate(ringModel);
         newRing.transform.position = new Vector3(pos.x, 0.11f, pos.z);
         newRing.GetComponent<RingViz>().lifespan = lifespan;
-        newRing.GetComponent<RingViz>().scale = .02f / (1 / volume);
+        newRing.GetComponent<RingViz>().scale = .02f / (.3f / volume);
         return newRing;
     }
 
     GameObject CreateSphere(int lifespan)
     {
         Vector3 pos = gameObject.transform.position;
-        float volume = audioSource.volume;
+        float volume = volumeGrabber.postLoudness();
         GameObject newSphere = Instantiate(sphereModel);
         newSphere.transform.position = new Vector3(pos.x, pos.y, pos.z);
         newSphere.transform.localScale = new Vector3(scale, scale, scale);
         newSphere.GetComponent<SphereViz>().lifespan = lifespan;
-        newSphere.GetComponent<SphereViz>().scale = .02f / (1 / volume);
+        newSphere.GetComponent<SphereViz>().scale = .02f / (.3f / volume);
         return newSphere;
     }
 }
