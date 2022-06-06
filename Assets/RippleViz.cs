649285c8923ff972275b33916e8fcb78d3e4d7a4
Daisy <penguinsliding4@gmail.com>
2022-06-06 01:34:39 -0700

    handle multi mesh, scaling

--- Assets/RippleViz.cs
+++ Assets/RippleViz.cs
@@ -17,7 +17,8 @@ public class RippleViz : MonoBehaviour
     private int activeSpheres = 0;
     private Queue<GameObject> rings = new Queue<GameObject>();
     private Queue<GameObject> spheres = new Queue<GameObject>();
-    private float scale;
+    private float ringScale;
+    private float sphereScale;
     private AudioSource audioSource;
     private SoundVolumeGrabber volumeGrabber;
 
@@ -28,8 +29,27 @@ public class RippleViz : MonoBehaviour
         ringModel = GameObject.Find("Ring");
         sphereModel = GameObject.Find("Sphere");
         rippleOn = false;
-        Bounds bounds = gameObject.GetComponent<Renderer>().bounds;
-        scale = (new float[] { bounds.size.x, bounds.size.y, bounds.size.z }).Max()*1.5f;
+
+        Bounds bounds;
+        if (gameObject.TryGetComponent(out Renderer renderer))
+        {
+            bounds = renderer.bounds;
+        } else
+        {
+            Renderer[] renderers = gameObject.GetComponentsInChildren<Renderer>();
+            if (renderers.Length == 1)
+            {
+                bounds = renderers[0].bounds;
+                print(bounds.size);
+
+            } else
+            {
+                bounds = CalcMultiBounds(renderers);
+            }
+        }
+        ringScale = (new float[] { bounds.size.x, bounds.size.z }).Min() * .5f;
+        sphereScale = (new float[] { bounds.size.x, bounds.size.y, bounds.size.z }).Max() * 1.5f;
+        
         audioSource = gameObject.GetComponent<AudioSource>();
         volumeGrabber = gameObject.GetComponent<SoundVolumeGrabber>();
         
@@ -137,8 +157,9 @@ public class RippleViz : MonoBehaviour
         float volume = volumeGrabber.postLoudness();
         GameObject newRing = Instantiate(ringModel);
         newRing.transform.position = new Vector3(pos.x, 0.11f, pos.z);
+        newRing.transform.localScale = new Vector3(ringScale, ringScale, ringScale);
         newRing.GetComponent<RingViz>().lifespan = lifespan;
-        newRing.GetComponent<RingViz>().scale = .02f / (.3f / volume);
+        newRing.GetComponent<RingViz>().scale = Mathf.Max(.02f / (.3f / volume), .005f);
         return newRing;
     }
 
@@ -148,11 +169,26 @@ public class RippleViz : MonoBehaviour
         float volume = volumeGrabber.postLoudness();
         GameObject newSphere = Instantiate(sphereModel);
         newSphere.transform.position = new Vector3(pos.x, pos.y, pos.z);
-        newSphere.transform.localScale = new Vector3(scale, scale, scale);
+        newSphere.transform.localScale = new Vector3(sphereScale, sphereScale, sphereScale);
         Debug.Log("1");
         newSphere.GetComponent<SphereViz>().lifespan = lifespan;
         Debug.Log("2");
-        newSphere.GetComponent<SphereViz>().scale = .02f / (.3f / volume);
+        newSphere.GetComponent<SphereViz>().scale = Mathf.Max(.02f / (.3f / volume), .005f);
         return newSphere;
+    }
+
+    Bounds CalcMultiBounds(Renderer[] renderers)
+    {
+        Quaternion currentRotation = gameObject.transform.rotation;
+        gameObject.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+
+        Bounds bounds = new Bounds(gameObject.transform.position, Vector3.zero);
+        foreach (Renderer renderer in renderers)
+        {
+            bounds.Encapsulate(renderer.bounds);
+        }
+
+        gameObject.transform.rotation = currentRotation;
+        return bounds;
     }
 }
